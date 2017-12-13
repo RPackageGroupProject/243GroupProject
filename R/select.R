8260245b6a4ba8c6dd751c4779157ee3f4b238b5
Christopher Gagne <cgagne@berkeley.edu>
2017-12-12 21:02:45 -0800

    changed

--- R/select.R
+++ R/select.R
@@ -61,7 +61,6 @@ select <- function(dat,
   # Number of variables, same as chromosome length
   C <- ncol(dat) - 1
 
-  print()
   # Make default inputs
   if (is.null(P)) {P <- as.integer(1.5 * C)}
   if (is.null(numGens)) {numGens <- 100}
